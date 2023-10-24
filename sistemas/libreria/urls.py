2434bd8d5bd6f9733a3649244ffd87ca4cbbbf98
Gerson Pereira <gerson.pereira2107@gmail.com>
2023-10-23 22:47:13 -0500

    Se agregaron modo administrador

--- sistemas/libreria/urls.py
+++ sistemas/libreria/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from  . import views
+from . import views
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
-    path('nosotros', views.nosotros, name='nosotros'),
-    path('juegos', views.juegos, name='juegos'),
-    path('juegos/crear', views.crear, name='crear'),
-    path('juegos/editar', views.editar, name='editar'),
-]
+    path("", views.inicio, name="inicio"),
+    path("nosotros", views.nosotros, name="nosotros"),
+    path("juegos", views.juegos, name="juegos"),
+    path("juegos/crear", views.crear, name="crear"),
+    path("juegos/editar", views.editar, name="editar"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- sistemas/libreria/views.py
+++ sistemas/libreria/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Juego
+
 # Create your views here.
 
+
 def inicio(request):
-    return render(request, 'paginas/inicio.html')
+    return render(request, "paginas/inicio.html")
+
+
 def nosotros(request):
-    return render(request, 'paginas/nosotros.html')
+    return render(request, "paginas/nosotros.html")
+
 
 def juegos(request):
-    return render(request, 'juegos/index.html')
+    juegos = Juego.objects.all()
+    return render(request, "juegos/index.html", {"juegos": juegos})
+
+
 def crear(request):
-    return render(request, 'juegos/crear.html')
+    return render(request, "juegos/crear.html")
+
+
 def editar(request):
-    return render(request, 'juegos/editar.html')
+    return render(request, "juegos/editar.html")
